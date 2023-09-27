1c8cb82d0cb360ca51781ce8340d4137ab860e76
dmitry <example.com>
2023-09-27 17:35:50 +0300

    реализовал task2

--- robots/tests.py
+++ robots/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from datetime import timedelta
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Robot
+
+
+class GetOrdersViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.get_robots = reverse('get')
+
+        Robot.objects.create(model='R2', version='D2', created=timezone.now() - timedelta(days=8))
+        Robot.objects.create(model='R3', version='D3', created=timezone.now() - timedelta(days=6))
+
+    def test_response_content_filename(self):
+        response = self.client.get(self.get_robots)
+        self.assertEquals(response['content-type'], 'application/vnd.ms-excel')
+        self.assertIn('filename=last_7_days_robots.xlsx', response['Content-Disposition'])
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+import collections
+import datetime
 
-# Create your views here.
+import openpyxl
+from django.db.models import Count
+from django.http import HttpResponse
+from django.utils import timezone
+from openpyxl.utils import get_column_letter
+
+from .models import Robot
+
+
+def get_orders(request):
+    start_datetime = timezone.now() - datetime.timedelta(days=7)
+    data = Robot.objects.filter(created__gte=start_datetime).values('model', 'version').annotate(count=Count('id'))
+
+    grouped_data = collections.defaultdict(list)
+    for item in data:
+        grouped_data[item['model']].append((item['version'], item['count']))
+
+    workbook = openpyxl.Workbook()
+    del workbook['Sheet']
+    header = ['Модель', 'Версия', 'Количество за неделю']
+
+    for model, versions_data in grouped_data.items():
+        worksheet = workbook.create_sheet(title=model)
+
+        for col_num, column_title in enumerate(header, 1):
+            column_letter = get_column_letter(col_num)
+            worksheet[f'{column_letter}1'] = column_title
+
+        for row_num, (version, count) in enumerate(versions_data, 2):
+            worksheet.cell(row=row_num, column=1, value=model)
+            worksheet.cell(row=row_num, column=2, value=version)
+            worksheet.cell(row=row_num, column=3, value=count)
+
+        for column_cells in worksheet.columns:
+            max_length = 0
+            column = column_cells[0].column_letter
+            for cell in column_cells:
+                try:
+                    if len(str(cell.value)) > max_length:
+                        max_length = len(cell.value)
+                except:
+                    pass
+            adjusted_width = (max_length + 2)
+            worksheet.column_dimensions[column].width = adjusted_width
+
+    if data:
+        response = HttpResponse(content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename=last_7_days_robots.xlsx'
+        workbook.save(response)
+    else:
+        response = HttpResponse('<h2>За последние 7 дней роботов не создавалось</h2>')
+
+    return response
